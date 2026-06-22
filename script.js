4cdca05e55d1f237b85bb538926327a015f5cd1b
22githrsh <senh2675@gmail.com>
2026-06-22 09:48:56 +0530

    push

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+let lastscroll = 0;
+
+window.addEventListener("scroll", () => {
+    let currentscroll = window.pageYOffset;
+
+    if (currentscroll > 300) {
+
+        if (currentscroll > lastscroll) {
+            gsap.to("header", { y: -100 });
+        }
+         else {
+            gsap.to("header", { y: 0 });
+        }
+
+    } 
+    else {
+        gsap.to("header", { y: 0 });
+    }
+
+    lastscroll = currentscroll;
+});
+
+let tl = gsap.timeline({
+    scrollTrigger:{
+        trigger:".about",
+        start:'top top',
+        end:"+=800",
+        scrub:2,
+        pin:true,
+        markers:true
+    }
+})
+
+tl.to(".about svg",{
+    scale:"0.6",
+    filter:"blur(30px)"
+})
+
+
