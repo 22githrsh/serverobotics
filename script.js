4318304be794dfd75b555b7775b35cf2ee86b811
22githrsh <senh2675@gmail.com>
2026-06-23 13:07:51 +0530

    pused

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// let lastscroll = 0;
-
-// window.addEventListener("scroll", () => {
-//     let currentscroll = window.pageYOffset;
-
-//     if (currentscroll > 300) {
-
-//         if (currentscroll > lastscroll) {
-//             gsap.to("header", { y: -100 });
-//         } else {
-//             gsap.to("header", { y: 0 });
-//         }
-
-//     } else {
-//         gsap.to("header", { y: 0 });
-//     }
-
-//     lastscroll = currentscroll;
-// });
-
-
-
 let lastscroll = 0 
 
 window.addEventListener("scroll",()=>{
@@ -42,30 +20,15 @@ window.addEventListener("scroll",()=>{
         })
      }
 lastscroll = currentscroll
-    
 })
 
-
-
-
-
-
-
-
-
-
-
-
-let h1 = document.querySelector(".about h1");
-
-let clutter = "";
+let h1 = document.querySelector(".about h1")
+let clutter = ""
 
 h1.textContent.split(" ").forEach(word => {
-    clutter += `<span>${word}</span> `;
+    clutter += ` <span>${word}</span> `
 });
-
-h1.innerHTML = clutter;
-
+h1.innerHTML = clutter
 
 
 let tl = gsap.timeline({
@@ -90,4 +53,24 @@ tl.from(".about h1 span",{
     y:20,
     stagger:0.15,
 
-}, 0.5);
+}, "<50%");
+
+// let tl = gsap.timeline({
+//     scrollTrigger:{
+//         trigger:".about",
+//         markers:true,
+//         start:"top top",
+//         end:"+=500",
+//         pin:true,
+//         scrub:2
+//     }
+// })
+// tl.to(".about svg",{
+//     scale:0.7,
+//     filter:"blur(30px)"
+// })
+// tl.to(".about h1",{
+//     opacity:1
+// },"<50%")
+
+
